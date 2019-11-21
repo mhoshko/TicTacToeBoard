68eae7a01c4523145c30783c65527c6163b765c2
mhoshko <madeline.hoshko@gmail.com>
2019-11-21 15:53:29 -0800

    fixed get piece tests

--- TicTacToeBoardTest.cpp
+++ TicTacToeBoardTest.cpp
@@ -63,7 +63,7 @@ TEST(TicTacToeBoardTest, checkgetpieceplaced){
 	ASSERT_EQ(board.getPiece(0, 0), X);
 }
 
-TEST(TicTacToeBoardTest, checkgetpieceplaced){
+TEST(TicTacToeBoardTest, checkgetpieceincorrectlyplaced){
 	TicTacToeBoard board;
 	board.placePiece(0,0);
 	board.placePiece(0,0)
